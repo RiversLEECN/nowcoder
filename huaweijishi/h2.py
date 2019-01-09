d041dad66c387a2c998eda9aaffdc6b3e5908327
RiversLEECN <lixiaojiang_py@163.com>
2019-01-09 15:17:15 +0800

    计算字符个数

---
+++ huaweijishi/h2.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+input_zfc=list(input().upper())
+input2_zf=input().upper()
+count=0
+for i in range(len(input_zfc)):
+    if input2_zf==input_zfc[i]:
+        count+=1
+print(count)
+
+
+
+
